007a735629078c1d9ece4f706e0c4d138b8dd712
Mehmet Şılak <silakmehmet@gmail.com>
2024-09-17 16:15:44 +0300

    Reservation operations restricted for users to access only their own, except admin

--- api/rent_a_car/permissions.py
+++ api/rent_a_car/permissions.py
@@ -9,3 +9,16 @@ class IsAdminOrReadOnly(BasePermission):
             return True
         # Giving full access for only admin users
         return bool(request.user and request.user.is_staff)
+
+
+class IsOwnerOrAdmin(BasePermission):
+    # Allowing only authenticated users
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        # Allowing admin for CRUD operations for all the reservations
+        if request.user and request.user.is_staff:
+            return True
+        # Allowing users for CRUD operations for only their own reservations
+        return obj.customer == request.user
--- api/rent_a_car/views.py
+++ api/rent_a_car/views.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Car, Reservation
 from .serializers import CarSerializer, ReservationSerializer
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrAdmin
 
 
 class CarMVS(ModelViewSet):
@@ -14,6 +14,15 @@ class CarMVS(ModelViewSet):
 class ReservationMVS(ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_staff:
+            return Reservation.objects.all()
+
+        return Reservation.objects.filter(customer=user)
 
     def perform_create(self, serializer):
         # Automatically setting the customer field for non-admin authenticated users
