c44625464bd61c38517b6c99c274d8b10e4b41e3
Mehmet Şılak <silakmehmet@gmail.com>
2024-09-17 12:31:04 +0300

    total_price, car and customer(names) fields added to ReservationSerializer

--- api/rent_a_car/serializers.py
+++ api/rent_a_car/serializers.py
@@ -11,7 +11,18 @@ class CarSerializer(serializers.ModelSerializer):
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+    car = serializers.StringRelatedField(read_only=True)
+    car_id = serializers.IntegerField()
+    customer = serializers.StringRelatedField(read_only=True)
+    customer_id = serializers.IntegerField()
+
     class Meta:
         model = Reservation
         fields = "__all__"
-        read_only_fields = ["id", "created_date", "updated_date"]
+        read_only_fields = ["id", "created_date",
+                            "updated_date", "total_price", "car", "customer"]
+
+    def get_total_price(self, obj):
+        reserved_days = (obj.end_date - obj.start_date).days+1
+        return reserved_days * obj.car.rent_per_day
